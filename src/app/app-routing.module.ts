11794f94815284eb00e5f9d45d4ad80b87c7e0d7
Janaina Teixeira <jaanagoncalves@gmail.com>
2022-05-10 11:08:08 -0300

    Revert "Crud tema"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
-import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
-import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { TemaComponent } from './tema/tema.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'entrar', pathMatch: 'full' },
-  { path: 'entrar', component: EntrarComponent },
-  { path: 'cadastrar', component: CadastrarComponent },
-  { path: 'inicio', component: InicioComponent },
-  { path: 'tema', component: TemaComponent },
-  { path: 'tema-edit/:id', component: TemaEditComponent },
-  { path: 'tema-delete/:id', component: TemaDeleteComponent },
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
+  {path: 'entrar', component: EntrarComponent},
+  {path: 'cadastrar', component: CadastrarComponent},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'tema', component: TemaComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
